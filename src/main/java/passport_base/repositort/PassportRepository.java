8b6594c5d4f4599e47d54c68f24454cc8b3a92bc
JavAndy58 <digital.designs.russia@gmail.com>
2022-08-24 17:49:54 +0300

    Init

--- src/main/java/passport_base/repositort/PassportRepository.java
+++ src/main/java/passport_base/repositort/PassportRepository.java
@@ -3,5 +3,6 @@ package passport_base.repositort;
 import org.springframework.data.repository.CrudRepository;
 import passport_base.model.Passport;
 
+
 public interface PassportRepository extends CrudRepository<Passport, Integer> {
 }
